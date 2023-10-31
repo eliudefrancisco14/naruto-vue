f6c5e39c6f9a4f04e043fb8aef6c309989fd339b
eliudefrancisco14 <eliudepauloquintasf@gmail.com>
2023-10-31 16:53:01 +0100

    Já estou consumindo a API do Naruto

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div v-if="dataApi" class="col-md-6">
+        <div v-for="data in dataApi.kekkeigenkai" :key="data.id">
+          <ul>
+            <li >{{ data.name }}</li>
+            <img :src="data.characters[0].images[0]" :alt="data.name">
+          </ul>
+        </div>
+      </div>
+      <div v-else class="col-md-6">
+        Loading...
+      </div>
+    </div>
+    
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data(){
+    return {
+      dataApi: null,
+    }
+  },
+  async mounted() {
+    try {
+      const response = await axios.get("kekkei-genkai?page=1&limit=5");
+      this.dataApi = response.data;
+      console.log(response);
+    } catch (error) {
+      console.error("Erro na solicitação HTTP:", error);
+    }
+  },
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap"
+import '@/service/config'
 
 createApp(App).mount('#app')
